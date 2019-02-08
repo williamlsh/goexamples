1892d361dd9f56abae75feb22d8cceb259d76aff
Willaim <1340440725fhs@gmail.com>
2019-02-08 19:42:01 +0800

    explicit cancellation by closed channel signal

--- piplines.go
+++ piplines.go
@@ -8,43 +8,52 @@ import (
 // Consider a pipeline with three stages.
 
 // The first stage, gen, is a function that converts a list of integers to a channel that emits the integers in the list.
-func gen(nums ...int) <-chan int {
+func gen(done <-chan struct{}, nums ...int) <-chan int {
 	out := make(chan int, len(nums)) // use buffered channel to avoid creating new goroutine.
 	for _, n := range nums {
-		out <- n
+		select {
+		case out <- n:
+		case <-done:
+			break
+		}
 	}
 	close(out)
 	return out
 }
 
 // The second stage, sq, receives integers from a channel and returns a channel that emits the square of each received integer.
-func sq(in <-chan int) <-chan int {
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
+		defer close(out)
 		for n := range in {
-			out <- n * n
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
 		}
-		close(out)
 	}()
 	return out
 }
 
 func main() {
+	// Set up a done channel that's shared by the whole pipeline,
+	// and close that channel when this pipeline exits, as a signal
+	// for all the goroutines we started to exit.
+	done := make(chan struct{})
+	defer close(done)
+
 	// Set up the pipeline and runs the final stage.
-	in := gen(2, 3)
+	in := gen(done, 2, 3)
 
 	// Distribute the sq work across two goroutines that both read from in.
-	c1 := sq(in)
-	c2 := sq(in)
+	c1 := sq(done, in)
+	c2 := sq(done, in)
 
 	// Consume the first value from output.
-	done := make(chan struct{}, 2)
 	out := merge(done, c1, c2)
 	fmt.Println(<-out)
-
-	// Tell the remianing senders we are leaving.
-	done <- struct{}{}
-	done <- struct{}{}
 }
 
 func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
@@ -55,13 +64,14 @@ func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	// copies values from c to out until c is closed or it receives a value
 	// from done, then calls wg.Done.
 	output := func(c <-chan int) {
+		defer wg.Done()
 		for n := range c {
 			select {
 			case out <- n:
-			case <-done: // stop sending to out channel in case of its blocking with non-receiving when main exists
+			case <-done: // return as soon as done is closed without draining its inbound channel
+				return
 			}
 		}
-		wg.Done()
 	}
 
 	// Start a goroutine to close out once all the output goroutines are
