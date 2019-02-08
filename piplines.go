e672d2ab600c0b59e1f57c43c60ebfd77120fff1
Willaim <1340440725fhs@gmail.com>
2019-02-08 15:45:37 +0800

    fan-out, fan-in

--- piplines.go
+++ piplines.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // Consider a pipeline with three stages.
 
@@ -30,10 +33,41 @@ func sq(in <-chan int) <-chan int {
 
 func main() {
 	// Set up the pipeline and runs the final stage.
-	c := gen(2, 3)
-	out := sq(c)
+	in := gen(2, 3)
 
-	// Consume the output.
-	fmt.Println(<-out)
-	fmt.Println(<-out)
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// Consume the merged output from c1 and c2.
+	for n := range merge(c1, c2) {
+		fmt.Println(n)
+	}
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
