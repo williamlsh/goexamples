3d587889f103b61e8546db357ac7cd69b1524de0
Willaim <1340440725fhs@gmail.com>
2019-02-09 12:38:36 +0800

    parallel tree digestion

--- serial.go
+++ serial.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"crypto/md5"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"sort"
+	"sync"
 )
+
+type result struct {
+	path string
+	sum  [md5.Size]byte
+	err  error
+}
 
 func main() {
 	// Calculate the MD5 sum of all files under the specified directory,
@@ -27,26 +35,69 @@ func main() {
 	}
 }
 
+func sumFiles(done <-chan struct{}, root string) (<-chan result, <-chan error) {
+	// For each regular file, start a goroutine that sums the file and sends
+	// the result on c.  Send the result of the walk on errc.
+	c := make(chan result)
+	errc := make(chan error, 1)
+	go func() {
+		var wg sync.WaitGroup
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+			wg.Add(1)
+			go func() {
+				data, err := ioutil.ReadFile(path)
+				select {
+				case c <- result{path, md5.Sum(data), err}:
+				case <-done:
+				}
+				wg.Done()
+			}()
+			// Abort the walk if done is closed.
+			select {
+			case <-done:
+				return errors.New("walk cancelled")
+			default:
+				return nil
+			}
+		})
+
+		// Walk has returned, so all calls to wg.Add are done.  Start a
+		// goroutine to close c once all the sends are done.
+		go func() {
+			wg.Wait()
+			close(c)
+		}()
+
+		// No select needed here, since errc is buffered.
+		errc <- err
+	}()
+	return c, errc
+}
+
 // MD5All reads all the files in the file tree rooted at root and returns a map
 // from file path to the MD5 sum of the file's contents.  If the directory walk
 // fails or any read operation fails, MD5All returns an error.
 func MD5All(root string) (map[string][md5.Size]byte, error) {
+	// MD5All closes the done channel when it returns; it may do so before
+	// receiving all the values from c and errc.
+	done := make(chan struct{})
+	defer close(done)
+
+	c, errc := sumFiles(done, root)
+
 	m := make(map[string][md5.Size]byte)
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+	for r := range c {
+		if err := r.err; err != nil {
+			return nil, err
 		}
-		if !info.Mode().IsRegular() {
-			return nil
-		}
-		data, err := ioutil.ReadFile(path)
-		if err != nil {
-			return err
-		}
-		m[path] = md5.Sum(data)
-		return nil
-	})
-	if err != nil {
+	}
+	if err := <-errc; err != nil {
 		return nil, err
 	}
 	return m, nil
