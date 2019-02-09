8c52bc5ee0bcaa87a1b87a3c6c47c9e261c060e7
Willaim <1340440725fhs@gmail.com>
2019-02-09 17:51:09 +0800

    bounded parallism

--- bounded.go
+++ bounded.go
@@ -35,49 +35,39 @@ func main() {
 	}
 }
 
-func sumFiles(done <-chan struct{}, root string) (<-chan result, <-chan error) {
-	// For each regular file, start a goroutine that sums the file and sends
-	// the result on c.  Send the result of the walk on errc.
-	c := make(chan result)
+// The first stage, walkFiles, emits the paths of regular files in the tree.
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
 	errc := make(chan error, 1)
 	go func() {
-		var wg sync.WaitGroup
-		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		defer close(paths)
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
 			if !info.Mode().IsRegular() {
 				return nil
 			}
-			wg.Add(1)
-			go func() {
-				data, err := ioutil.ReadFile(path)
-				select {
-				case c <- result{path, md5.Sum(data), err}:
-				case <-done:
-				}
-				wg.Done()
-			}()
-			// Abort the walk if done is closed.
 			select {
+			case paths <- path:
 			case <-done:
 				return errors.New("walk cancelled")
-			default:
-				return nil
 			}
+			return nil
 		})
-
-		// Walk has returned, so all calls to wg.Add are done.  Start a
-		// goroutine to close c once all the sends are done.
-		go func() {
-			wg.Wait()
-			close(c)
-		}()
-
-		// No select needed here, since errc is buffered.
-		errc <- err
 	}()
-	return c, errc
+	return paths, errc
+}
+
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result) {
+	for path := range paths {
+		data, err := ioutil.ReadFile(path)
+		select {
+		case c <- result{path, md5.Sum(data), err}:
+		case <-done:
+			return
+		}
+	}
 }
 
 // MD5All reads all the files in the file tree rooted at root and returns a map
@@ -89,14 +79,33 @@ func MD5All(root string) (map[string][md5.Size]byte, error) {
 	done := make(chan struct{})
 	defer close(done)
 
-	c, errc := sumFiles(done, root)
+	paths, errc := walkFiles(done, root)
+
+	// Start a fixed number of goroutines to read and digest files.
+	c := make(chan result) // HLc
+	var wg sync.WaitGroup
+	const numDigesters = 20
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, paths, c) // HLc
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c) // HLc
+	}()
+	// End of pipline.
 
 	m := make(map[string][md5.Size]byte)
 	for r := range c {
 		if err := r.err; err != nil {
 			return nil, err
 		}
+		m[r.path] = r.sum
 	}
+	// Check whether the Walk failed.
 	if err := <-errc; err != nil {
 		return nil, err
 	}
