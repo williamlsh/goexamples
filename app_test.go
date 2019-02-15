0ec73776f18d802ede04d2e814247fdbcf530d3e
Willaim <1340440725fhs@gmail.com>
2019-02-15 19:28:15 +0800

    added more tests

--- app_test.go
+++ app_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -30,4 +32,94 @@ func TestBlogsList(t *testing.T) {
 	fmt.Println("============ jsonapi response from list ===========")
 	fmt.Println(string(jsonReply))
 	fmt.Println("============== end raw jsonapi from list =============")
+}
+
+func TestBlogShow(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "/blogs?id=1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set(headerAccept, jsonapi.MediaType)
+
+	w := httptest.NewRecorder()
+	handler := &ExampleHandler{}
+
+	fmt.Println("============ start show ===========")
+	handler.ServeHTTP(w, req)
+	fmt.Println("============ stop show ===========")
+
+	jsonReply, _ := ioutil.ReadAll(w.Body)
+
+	fmt.Println("============ jsonapi response from show ===========")
+	fmt.Println(string(jsonReply))
+	fmt.Println("============== end raw jsonapi from show =============")
+}
+
+func TestBlogCreate(t *testing.T) {
+	blog := fixtureBlogCreate(1)
+	in := new(bytes.Buffer)
+	jsonapi.MarshalOnePayloadEmbedded(in, blog)
+
+	req, err := http.NewRequest(http.MethodGet, "/blogs", in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set(headerAccept, jsonapi.MediaType)
+
+	w := httptest.NewRecorder()
+	handler := &ExampleHandler{}
+
+	fmt.Println("============ start create ===========")
+	handler.ServeHTTP(w, req)
+	fmt.Println("============ stop create ===========")
+
+	jsonReply, _ := ioutil.ReadAll(w.Body)
+
+	fmt.Println("============ jsonapi response from create ===========")
+	fmt.Println(string(jsonReply))
+	fmt.Println("============== end raw jsonapi from create =============")
+}
+
+func TestBlogEcho(t *testing.T) {
+	blogs := []interface{}{
+		fixtureBlogCreate(1),
+		fixtureBlogCreate(2),
+		fixtureBlogCreate(3),
+	}
+	in := new(bytes.Buffer)
+	jsonapi.MarshalOnePayloadEmbedded(in, blogs)
+
+	req, err := http.NewRequest(http.MethodGet, "/blogs", in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set(headerAccept, jsonapi.MediaType)
+
+	w := httptest.NewRecorder()
+	handler := &ExampleHandler{}
+
+	fmt.Println("============ start create ===========")
+	handler.ServeHTTP(w, req)
+	fmt.Println("============ stop create ===========")
+
+	jsonReply, _ := ioutil.ReadAll(w.Body)
+
+	fmt.Println("============ jsonapi response from create ===========")
+	fmt.Println(string(jsonReply))
+	fmt.Println("============== end raw jsonapi from create =============")
+
+	responseBlog := new(Blog)
+
+	buf := new(bytes.Buffer)
+	jsonapi.UnmarshalPayload(buf, responseBlog)
+
+	out := bytes.NewBuffer(nil)
+	json.NewEncoder(out).Encode(responseBlog)
+
+	fmt.Println("================ Viola! Converted back our Blog struct =================")
+	fmt.Println(string(out.Bytes()))
+	fmt.Println("================ end marshal materialized Blog struct =================")
 }
