38940b2e490c754bd81bed38dafac45d69f46976
Willaim <1340440725fhs@gmail.com>
2019-02-22 17:44:24 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+// Reference: https://medium.com/weareservian/automagical-https-with-docker-and-go-4953fdaf83d2
+package main
+
+import (
+	"crypto/tls"
+	"io"
+	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello, world")
+	})
+
+	certManager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("secret-dir"),
+		// Put your domain here:
+		HostPolicy: autocert.HostWhitelist("example.com"),
+	}
+
+	server := http.Server{
+		Addr:    ":https",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	server.ListenAndServeTLS("", "")
+}
