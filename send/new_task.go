ebe1db06ae411b6d157d66fa2fed40723fabb88e
William <webdevelopper@protonmail.ch>
2019-04-15 17:19:26 +0800

    minor changes

--- send/new_task.go
+++ send/new_task.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/streadway/amqp"
 )
@@ -10,6 +12,16 @@ func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err.Error())
 	}
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if len(args) < 2 || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], "")
+	}
+	return s
 }
 
 func main() {
@@ -34,16 +46,18 @@ func main() {
 	)
 	failOnError(err, "failed to declare a queue")
 
-	body := "Hello World"
+	body := bodyFrom(os.Args)
 	err = ch.Publish(
 		"",     // exchange
 		q.Name, // routing key
 		false,  // mandatory
 		false,  // immediate
 		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(body),
 		},
 	)
 	failOnError(err, "failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
 }
