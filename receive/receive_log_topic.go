93c99374d1ee97b5d4b57035bb7c4f4832894b3f
William <webdevelopper@protonmail.ch>
2019-04-16 11:33:13 +0800

    Minor changes to topic subscribe

--- receive/receive_log_topic.go
+++ receive/receive_log_topic.go
@@ -23,13 +23,13 @@ func main() {
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"logs_direct", // name
-		"direct",      // type
-		true,          // durable
-		false,         // auto-deleted
-		false,         // internal
-		false,         // no-wait
-		nil,           // arguments
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 
@@ -49,13 +49,13 @@ func main() {
 	}
 
 	for _, s := range os.Args[1:] {
-		log.Printf("Binding queue %s to exchange %s with routing key %s", q.Name, "logs_direct", s)
+		log.Printf("Binding queue %s to exchange %s with routing key %s", q.Name, "logs_topic", s)
 
 		// Bindings.
 		err = ch.QueueBind(
-			q.Name,        // queue name
-			s,             // routing key
-			"logs_direct", // exchange
+			q.Name,       // queue name
+			s,            // routing key
+			"logs_topic", // exchange
 			false,
 			nil,
 		)
