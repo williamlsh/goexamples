f96f12e9303279dc044b237b0c6508835a910d47
William <webdevelopper@protonmail.ch>
2019-05-13 21:22:40 +0800

    Bounded parallelism

--- main.go
+++ main.go
@@ -28,53 +28,44 @@ func main() {
 	}
 }
 
-type result struct {
-	path string
-	sum  [md5.Size]byte
-	err  error
-}
-
-func sumFiles(done <-chan struct{}, root string) (<-chan result, <-chan error) {
-	// For each regular file, start a goroutine that sums the file and sends
-	// the result on c.  Send the result of the walk on errc.
-	c := make(chan result)
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
 	errc := make(chan error, 1)
 	go func() {
-		var wg sync.WaitGroup
-		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		defer close(paths)
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
 			if !info.Mode().IsRegular() {
 				return nil
 			}
-			wg.Add(1)
-			go func() {
-				data, err := ioutil.ReadFile(path)
-				select {
-				case c <- result{path: path, sum: md5.Sum(data), err: err}:
-				case <-done:
-				}
-				wg.Done()
-			}()
-			// Abort the walk if done is closed.
 			select {
+			case paths <- path:
 			case <-done:
 				return errors.New("walk cancelled")
-			default:
-				return nil
 			}
+			return nil
 		})
-		// Walk has returned, so all calls to wg.Add are done.  Start a
-		// goroutine to close c once all the sends are done.
-		go func() {
-			wg.Wait()
-			close(c)
-		}()
-		// No select needed here, since errc is buffered.
-		errc <- err
 	}()
-	return c, errc
+	return paths, errc
+}
+
+type result struct {
+	path string
+	sum  [md5.Size]byte
+	err  error
+}
+
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result) {
+	for path := range paths {
+		data, err := ioutil.ReadFile(path)
+		select {
+		case c <- result{path, md5.Sum(data), err}:
+		case <-done:
+			return
+		}
+	}
 }
 
 // MD5All reads all the files in the file tree rooted at root and returns a map
@@ -86,14 +77,31 @@ func MD5All(root string) (map[string][md5.Size]byte, error) {
 	done := make(chan struct{})
 	defer close(done)
 
-	c, errc := sumFiles(done, root)
+	paths, errc := walkFiles(done, root)
+
+	c := make(chan result)
+	var wg sync.WaitGroup
+	const numDigesters = 20
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, paths, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
 	m := make(map[string][md5.Size]byte)
 	for r := range c {
-		if err := r.err; err != nil {
-			return nil, err
+		if r.err != nil {
+			return nil, r.err
 		}
 		m[r.path] = r.sum
 	}
+	// Check whether the Walk failed.
 	if err := <-errc; err != nil {
 		return nil, err
 	}
