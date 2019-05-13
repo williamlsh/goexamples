b93feb66c6211e9b73b32a384f97770c43af454b
William <webdevelopper@protonmail.ch>
2019-05-13 14:28:59 +0800

    simple pipeline

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	for n := range sq(sq(sq(gen(1, 2, 3, 4, 5, 6)))) {
+		fmt.Println(n)
+	}
+}
