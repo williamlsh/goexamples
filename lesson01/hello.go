20530af3901e498cc30af2767a16b32cb16fb5d6
William <gopherx@protonmail.ch>
2020-10-15 11:57:15 +0800

    Lesson 1 and lesson 2

---
+++ lesson01/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/opentracing/opentracing-go/log"
+
+	"github.com/williamlsh/goexamples/lib/tracing"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("Error: expecting one argument")
+	}
+
+	tracer, closer := tracing.Init("hello-world")
+	defer closer.Close()
+
+	helloTo := os.Args[1]
+
+	span := tracer.StartSpan("say-hello")
+	span.SetTag("hello-to", helloTo)
+
+	helloStr := fmt.Sprintf("Hello, %s!", helloTo)
+	span.LogFields(
+		log.String("event", "string-format"),
+		log.String("value", helloStr),
+	)
+
+	println(helloStr)
+	span.LogKV("event", "println")
+
+	span.Finish()
+}
---
+++ lib/tracing/init.go
@@ -0,0 +1,28 @@
+package tracing
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+)
+
+func Init(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName: service,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	return tracer, closer
+}
