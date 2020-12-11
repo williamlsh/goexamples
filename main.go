3924661752519ac4d810e4e7bd877caf86b6ae22
William <gopherx@protonmail.ch>
2020-12-11 19:40:21 +0800

    Optimaze

--- main.go
+++ main.go
@@ -2,8 +2,6 @@
 package main
 
 import (
-	"bytes"
-	"flag"
 	"image"
 	"image/color"
 	"image/draw"
@@ -12,13 +10,8 @@ import (
 	"net/http"
 )
 
-var root = flag.String("root", ".", "File system path")
-
 func main() {
-	flag.Parse()
-
 	http.HandleFunc("/black/", handleBlack)
-	http.Handle("/", http.FileServer(http.Dir(*root)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -27,23 +20,10 @@ func handleBlack(w http.ResponseWriter, r *http.Request) {
 	black := color.RGBA{0, 0, 0, 255}
 	draw.Draw(m, m.Bounds(), &image.Uniform{black}, image.Point{}, draw.Src)
 
-	var img image.Image = m
-	if err := writeImage(w, &img); err != nil {
+	w.Header().Set("Content-Type", "image/jpeg")
+
+	if err := jpeg.Encode(w, image.Image(m), nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-
-func writeImage(w http.ResponseWriter, img *image.Image) error {
-	buf := new(bytes.Buffer)
-	if err := jpeg.Encode(buf, *img, nil); err != nil {
-		return err
-	}
-
-	w.Header().Set("Content-Type", "image/jpeg")
-	if _, err := w.Write(buf.Bytes()); err != nil {
-		return err
-	}
-
-	return nil
 }
