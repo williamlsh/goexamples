c90b3e9dec4c15dd1b1326eebee81c22b88fb34d
William <gopherx@protonmail.ch>
2021-03-15 11:38:27 +0800

    Error logging with stacktrace

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/williamlsh/goexamples
+
+go 1.16
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/rs/zerolog v1.20.0
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+func main() {
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	err := outer()
+	log.Error().Stack().Err(err).Msg("")
+}
+
+func inner() error {
+	return errors.New("seems we have an error here")
+}
+
+func middle() error {
+	err := inner()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func outer() error {
+	err := middle()
+	if err != nil {
+		return err
+	}
+	return nil
+}
