43b009dfeb11375aa7e274452d1051917fc34f2b
Sean DuBois <sean@siobud.com>
2021-03-18 11:45:44 -0700

    Rewrite rpt-broadcast
    Mix of play-from-disk-renegotation and rtp-to-webrtc

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
 module github.com/williamlsh/goexamples
 
-go 1.16
+go 1.15
 
-require (
-	github.com/pion/rtcp v1.2.6
-	github.com/pion/webrtc/v3 v3.0.15
-)
+require github.com/pion/webrtc/v3 v3.0.15
---
+++ publisher/main.go
@@ -0,0 +1,103 @@
+// +build !js
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/pion/webrtc/v3"
+)
+
+func main() {
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a video track
+	videoTrack, err := webrtc.NewTrackLocalStaticRTP(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP8}, "video", "pion")
+	if err != nil {
+		panic(err)
+	}
+	rtpSender, err := peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read incoming RTCP packets
+	// Before these packets are returned they are processed by interceptors. For things
+	// like NACK this needs to be called.
+	go func() {
+		rtcpBuf := make([]byte, 1500)
+		for {
+			if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+				return
+			}
+		}
+	}()
+
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+	if err = peerConnection.SetLocalDescription(offer); err != nil {
+		panic(err)
+	}
+	<-gatherComplete
+
+	offerJSON, err := json.Marshal(*peerConnection.LocalDescription())
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := http.Post("http://localhost:8080/createPublisher", "application/json", bytes.NewBuffer(offerJSON))
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	answer := webrtc.SessionDescription{}
+	if err = json.NewDecoder(response.Body).Decode(&answer); err != nil {
+		panic(err)
+	}
+
+	if err = peerConnection.SetRemoteDescription(answer); err != nil {
+		panic(err)
+	}
+
+	// Open a UDP Listener for RTP Packets on port 5004
+	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 5004})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = listener.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Read RTP packets forever and send them to the WebRTC Client
+	inboundRTPPacket := make([]byte, 1600) // UDP MTU
+	for {
+		n, _, err := listener.ReadFrom(inboundRTPPacket)
+		if err != nil {
+			panic(fmt.Sprintf("error during read: %s", err))
+		}
+
+		if _, err = videoTrack.Write(inboundRTPPacket[:n]); err != nil {
+			panic(err)
+		}
+	}
+}
