7a2261de60e654c04ca9d42e8321c0cfc10afcf8
William <gopherx@protonmail.ch>
2021-03-18 19:00:33 +0800

    Pasted official example code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/williamlsh/goexamples
+
+go 1.16
+
+require (
+	github.com/pion/rtcp v1.2.6
+	github.com/pion/webrtc/v3 v3.0.15
+)
---
+++ signalling/signalling.go
@@ -0,0 +1,134 @@
+package signalling
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// HTTPSDPServer starts a HTTP Server that consumes SDPs
+func HTTPSDPServer() chan string {
+	port := flag.Int("port", 8080, "http server port")
+	flag.Parse()
+
+	sdpChan := make(chan string)
+	http.HandleFunc("/sdp", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Fprintf(w, "done")
+		sdpChan <- string(body)
+	})
+
+	go func() {
+		err := http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	return sdpChan
+}
+
+// Allows compressing offer/answer to bypass terminal input limits.
+const compress = false
+
+// MustReadStdin blocks until input is received from stdin
+func MustReadStdin() string {
+	r := bufio.NewReader(os.Stdin)
+
+	var in string
+	for {
+		var err error
+		in, err = r.ReadString('\n')
+		if err != io.EOF {
+			if err != nil {
+				panic(err)
+			}
+		}
+		in = strings.TrimSpace(in)
+		if len(in) > 0 {
+			break
+		}
+	}
+
+	fmt.Println("")
+
+	return in
+}
+
+// Encode encodes the input in base64
+// It can optionally zip the input before encoding
+func Encode(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	if compress {
+		b = zip(b)
+	}
+
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// Decode decodes the input from base64
+// It can optionally unzip the input after decoding
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	if compress {
+		b = unzip(b)
+	}
+
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func zip(in []byte) []byte {
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	_, err := gz.Write(in)
+	if err != nil {
+		panic(err)
+	}
+	err = gz.Flush()
+	if err != nil {
+		panic(err)
+	}
+	err = gz.Close()
+	if err != nil {
+		panic(err)
+	}
+	return b.Bytes()
+}
+
+func unzip(in []byte) []byte {
+	var b bytes.Buffer
+	_, err := b.Write(in)
+	if err != nil {
+		panic(err)
+	}
+	r, err := gzip.NewReader(&b)
+	if err != nil {
+		panic(err)
+	}
+	res, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
