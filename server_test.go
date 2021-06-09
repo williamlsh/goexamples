8a3ccaef3a008f0fbf830154ed05f217cfee1ab6
William <gopherx@protonmail.ch>
2021-06-09 16:50:49 +0800

    Minor update

--- server_test.go
+++ server_test.go
@@ -10,20 +10,23 @@ func TestProcessQuery(t *testing.T) {
 	var cache Cache
 	t.Run("empty query", func(t *testing.T) {
 		var content []string
-		if result := processQuery(content, &cache); !reflect.DeepEqual(result, []bool{}) {
+		result := processQuery(content, &cache)
+		if !reflect.DeepEqual(result, []bool{}) {
 			t.Fatalf("unexpected result: %+v", result)
 		}
 		fmt.Printf("cache in test 1: %+v\n", cache.list)
 	})
 	t.Run("Two times query one hit", func(t *testing.T) {
 		content1 := []string{"a", "b", "c"}
-		if result := processQuery(content1, &cache); !reflect.DeepEqual(result, []bool{false, false, false}) {
-			t.Fatalf("unexpected result: %+v", result)
+		result1 := processQuery(content1, &cache)
+		if !reflect.DeepEqual(result1, []bool{false, false, false}) {
+			t.Fatalf("unexpected result: %+v", result1)
 		}
 
 		content2 := []string{"a", "c", "d"}
-		if result := processQuery(content2, &cache); !reflect.DeepEqual(result, []bool{true, false, false}) {
-			t.Fatalf("unexpected result: %+v", result)
+		result2 := processQuery(content2, &cache)
+		if !reflect.DeepEqual(result2, []bool{true, false, false}) {
+			t.Fatalf("unexpected result: %+v", result2)
 		}
 
 		if !reflect.DeepEqual(cache.list, []Set{
@@ -42,13 +45,15 @@ func TestProcessQuery(t *testing.T) {
 	})
 	t.Run("shorter query content all hits", func(t *testing.T) {
 		content := []string{"a", "b"}
-		if result := processQuery(content, &cache); !reflect.DeepEqual(result, []bool{true, true}) {
+		result := processQuery(content, &cache)
+		if !reflect.DeepEqual(result, []bool{true, true}) {
 			t.Fatalf("unexpected result: %+v", result)
 		}
 	})
 	t.Run("longger query content no hit", func(t *testing.T) {
 		content := []string{"e", "f", "g", "h", "i"}
-		if result := processQuery(content, &cache); !reflect.DeepEqual(result, []bool{false, false, false, false, false}) {
+		result := processQuery(content, &cache)
+		if !reflect.DeepEqual(result, []bool{false, false, false, false, false}) {
 			t.Fatalf("unexpected result: %+v", result)
 		}
 		fmt.Printf("cache: %+v\n", cache.list)
