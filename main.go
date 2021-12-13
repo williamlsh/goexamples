7dc26c5bbe3861014528226d71b21c8f62117787
William <gopherx@protonmail.ch>
2021-12-13 19:40:53 +0800

    hello world.

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	go func() {
+		if err := receive(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		if err := send(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	select {}
+}
+
+func send() error {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	if err = ch.Confirm(false); err != nil {
+		return err
+	}
+
+	confirmationCh := make(chan amqp.Confirmation, 1)
+	ch.NotifyPublish(confirmationCh)
+	go func() {
+		for c := range confirmationCh {
+			if !c.Ack {
+				fmt.Println("Server missed a published message.")
+			} else {
+				fmt.Println("Sent a message to server.")
+			}
+		}
+	}()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	body := "Hello, world!"
+	return ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{ // msg
+			Timestamp:   time.Now(),
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+}
+
+func receive() error {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		return err
+	}
+
+	for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+	}
+
+	return nil
+}
